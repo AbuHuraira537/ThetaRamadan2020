ac3e05c2b96fa125899b114261044d7721c7212b
AbuHuraira537 <abouhuraira967@gmail.com>
2020-07-13 11:52:24 +0500

    depandency injection injected

---
+++ ThetaRamadan2020/Models/PageViewCount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThetaRamadan2020.Models
+{
+    public partial class PageViewCount
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public int Count { get; set; }
+    }
+}
